ba5f69e2c7977245d0539cbdbc0bbe5e9cb96366
Amit Patel <amit@mramitpatel.com>
2014-09-30 09:02:16 -0500

    Removed iCheck and added svg method...
    Added svgcheckbox.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-  $('input').iCheck({
-    checkboxClass: 'icheckbox_square',
-    radioClass: 'iradio_square',
-    increaseArea: '20%' // optional
-  }).on('ifChecked', function(event){
-  alert(event.type + ' callback');
-  });
+
 });
