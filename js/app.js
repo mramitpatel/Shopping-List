ba62625328118767aa5d8a836455e5c70b92a1a2
Amit Patel <amit@mramitpatel.com>
2014-10-05 21:46:31 -0500

    Text-Field

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-
+  $(".delete").click(function(event){
+    $(event.target).closest("li").hide();
+    $(this).css("float:left");
+  });
+  var listitem = $("txt-list-input").text();
 });
