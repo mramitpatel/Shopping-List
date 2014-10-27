28657ef21533ce96e1963087ba5f56aed3b39689
Amit Patel <amit@mramitpatel.com>
2014-10-27 08:05:33 -0500

    Character length check

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
+
 $(document).ready(function(){
-  $(".delete").click(function(event){
-    $(event.target).closest("li").hide();
-    $(this).css("float:left");
-  });
-  var listitem = $("txt-list-input").text();
+	$("#add-value").click(function(){
+		var listItem = $("#txt-list-input").val();
+		if (listItem.length > 2){
+			$(".list").append('<li><input id="cb21" name="cb21" type="checkbox"><label for="cb21">'+ listItem +'</label><span class="delete">Delete</span></li>');
+				return false;
+				$("#txt-list-input").val("");
+		} else {
+			alert("need more characters yo");
+		}
+	});
+
+
+	$(".delete").click(function(){
+	  $(this).closest("li").remove();
+
+	});
 });
