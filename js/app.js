19219d89672cbfa3c04202ed3880a0b30f139c63
Amit Patel <amit@mramitpatel.com>
2014-10-28 22:26:28 -0500

    Local/Global variable & function scope

--- js/app.js
+++ js/app.js
@@ -1,20 +1,24 @@
 
+
 $(document).ready(function(){
 	$("#add-value").click(function(){
 		var listItem = $("#txt-list-input").val();
+			$("#txt-list-input").val(" ");
 		if (listItem.length > 2){
 			$(".list").append('<li><input id="cb21" name="cb21" type="checkbox"><label for="cb21">'+ listItem +'</label><span class="delete">Delete</span></li>');
 				return false;
-				$("#txt-list-input").val("");
 		} else {
 			alert("need more characters yo");
 		}
 	});
-
-
-	$(".delete").click(function(){
-	  $(this).closest("li").remove();
-	  alert("item deleted");
-
-	});
 });
+
+
+$(".list").on('click', '.delete', function(){
+	$(this).closest("li").remove();
+		alert("item deleted");
+});	
+
+$(".list").on('click', 'label', function(){
+	$(this).toggleClass("strike");
+})
