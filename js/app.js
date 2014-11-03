18c07e7062aff6fa66add7355648873ad2ac153d
Amit Patel <amit@mramitpatel.com>
2014-11-03 08:24:57 -0600

    Numerical list to static

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		var listItem = $("#txt-list-input").val();
 			$("#txt-list-input").val(" ");
 		if (listItem.length > 2){
-			$(".list").append('<li><input id="cb21" name="cb21" type="checkbox"><label for="cb21">'+ listItem +'</label><span class="delete">Delete</span></li>');
+			$(".list").append('<li><input id="cb1" name="cb1" type="checkbox"><label for="cb1">'+ listItem +'</label><span class="delete">Delete</span></li>');
 				return false;
 		} else {
 			alert("need more characters yo");
@@ -16,7 +16,6 @@ $(document).ready(function(){
 
 $(".list").on('click', '.delete', function(){
 	$(this).closest("li").remove();
-		alert("item deleted");
 });	
 
 $(".list").on('click', 'label', function(){
